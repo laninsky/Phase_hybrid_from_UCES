9ce9a2e45dfb2ca3389fe6047d65239b8bfe3a9d
Alana Alexander <laninsky@gmail.com>
2015-07-08 16:12:31 -0500

    Update extract_hybrid.R

--- phasing_step/extract_hybrid.R
+++ phasing_step/extract_hybrid.R
@@ -1,9 +1,10 @@
 intable <- read.table("temp",header=FALSE,stringsAsFactors=FALSE,sep="\t")
+variables <- read.table("phasing_settings",header=FALSE,stringsAsFactors=FALSE,sep="\t")
 
 rows <- dim(intable)[1]
 
-# Change no_taxa to 2 * the number of taxa you have, e.g. this is for 24 samples
-no_taxa <- 48
+no_taxa <- as.numeric(variables[3,1])*2
+hybridname <- variables[4,1]
 
 to_write <- matrix(NA,ncol=1,nrow=no_taxa)
 to_write[1,1] <- intable[1,1]
@@ -34,7 +35,7 @@ x <- 1
 j <- 1
 
 while (j < rows) {
-if ((length(grep("k_pix_e",to_write[j,1])))>0) {
+if ((length(grep(hybridname,to_write[j,1])))>0) {
 to_write_hybrid[1,1] <-  to_write[j,1]
 to_write_hybrid[2,1] <-  to_write[(j+1),1]
 } else {
@@ -46,6 +47,6 @@ j <- j + 2
 }
 
 write.table(to_write_temp, "temp.fa",quote=FALSE, col.names=FALSE,row.names=FALSE)
-write.table(to_write_hybrid, "hybrid_ref.fa",quote=FALSE, col.names=FALSE,row.names=FALSE)
+write.table(to_write_hybrid, "hybrid_ref.fa",quote=FALSE, col.names=FALSE,row.names=FALSE, append=TRUE)
 
 q()
