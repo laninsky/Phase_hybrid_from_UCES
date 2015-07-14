4bc522adfa4b9fef366e2aa2fe27342a7459b4af
Alana Alexander <laninsky@gmail.com>
2015-07-14 16:22:58 -0500

    Update remove_uncertains.R

--- post-processing/remove_uncertains.R
+++ post-processing/remove_uncertains.R
@@ -3,38 +3,38 @@ species <- read.table("species_assignments",header=FALSE,stringsAsFactors=FALSE)
 
 rows <- dim(intable)[1]
 
-no_final_taxa <- dim(species)[1] - sum(species[,2]=="uncertain")
-
-to_write <- matrix(NA,ncol=1,nrow=(no_final_taxa*2))
-
 uncertains <- species[species[,2]=="uncertain",1]
 no_uncertains <- length(uncertains)
 
-to_write_title <- 1
-
+to_write <- NULL
 sequencepaste <- NULL
+tempname <- NULL
 
 for (j in 1:rows) {
 if ((length(grep(">",intable[j,1])))>0) {
 if(!(any(uncertains %in% (gsub(">","",intable[j,1]))))) {
-if(is.null(sequencepaste)) {
-to_write[to_write_title,1] <- intable[j,1]
-to_write_title <- to_write_title + 1
-} else {
-to_write_seq <- to_write_title
-to_write_title <- to_write_title + 1
-to_write[to_write_seq,1] <- sequencepaste
-to_write[to_write_title,1] <- intable[j,1]
-to_write_title <- to_write_title + 1
+if(!(is.null(sequencepaste))) {
+if ((nchar(gsub("N","",gsub("-","",sequencepaste),ignore.case=T)))>0) {
+to_write <- rbind(to_write,tempname)
+to_write <- rbind(to_write,sequencepaste)
 }
 }
+}
+tempname <- intable[j,1]
 sequencepaste <- NULL
 } else {
 sequencepaste <- paste(sequencepaste,intable[j,1],sep="")
 }
 }
 
-to_write[(no_final_taxa*2),1] <- sequencepaste
+if ((nchar(gsub("N","",gsub("-","",sequencepaste),ignore.case=T)))>0) {
+to_write <- rbind(to_write,tempname)
+to_write <- rbind(to_write,sequencepaste)
+} 
+
+if ((length(grep(">",to_write[1,1])))==0) {
+to_write <- rbind(intable[1,1],to_write)
+}
 
 write.table(to_write, "temp.fa",quote=FALSE, col.names=FALSE,row.names=FALSE)
 
