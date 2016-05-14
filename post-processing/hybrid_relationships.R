3e69c71a853fa6cdeb0678ebac6e0d7cbd240709
Alana Alexander <laninsky@gmail.com>
2016-05-14 15:32:48 -0500

    Update hybrid_relationships.R

--- post-processing/hybrid_relationships.R
+++ post-processing/hybrid_relationships.R
@@ -6,6 +6,7 @@ suffixes <- as.numeric(as.matrix(read.table("suffix.txt")))
 
 lenintable <- dim(intable)[1]
 temptable <- NULL
+temptemptable <- NULL
 
 for (j in 1:lenintable) {
 temp <- unlist(strsplit(intable[j,2],"\\("))
@@ -16,17 +17,39 @@ if (coloncount==2) {
 temp2 <- unlist(strsplit(temp1,":"))
 temp3 <- unlist(strsplit(temp2,","))
 temp4 <- c(intable[j,1],temp3[1],temp3[3])
-temptable <- rbind(temptable,temp4)
+temptemptable <- rbind(temptemptable,temp4)
 }
 }
+colnames(temptemptable) <- NULL
+rownames(temptemptable) <- NULL
+sumtemptable <- as.data.table(temptemptable)[,c(2,3) := list(pmin(V2, V3), pmax(V2, V3))]
+sumtemptable$V2 <- substr(sumtemptable$V2,1, nchar(sumtemptable$V2)-suffixes)
+sumtemptable$V3 <- substr(sumtemptable$V3,1, nchar(sumtemptable$V3)-suffixes)
+samples <- matrix(NA,ncol=1,nrow=((dim(sumtemptable)[1])*2))
+samples[1:(dim(sumtemptable)[1]),1] <- sumtemptable$V2
+samples[((dim(sumtemptable)[1])+1):(dim(samples)[1]),1] <- sumtemptable$V3
+samplenames <- unique(samples)
+
+temp1table <- matrix(NA,ncol=4,nrow=(dim(samplenames)[1]))
+temp1table[,1] <- temptemptable[1,1]
+temp1table[,2] <- samplenames
+
+
+
+
+
+######## UP TO HERE RECODING ###########
+
+
+rbind(,sumtemptable$V3)
+
+
 }
 
 colnames(temptable) <- NULL
 rownames(temptable) <- NULL
 
 sumtable <- as.data.table(temptable)[,c(2,3) := list(pmin(V2, V3), pmax(V2, V3))]
-sumtable$V2 <- substr(sumtable$V2,1, nchar(sumtable$V2)-suffixes)
-sumtable$V3 <- substr(sumtable$V3,1, nchar(sumtable$V3)-suffixes)
 
 out <- ddply(sumtable,.(V2,V3),nrow)
 
