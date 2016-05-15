f69e24e258702c0a3da45d1b31a15f22c34db4f7
Alana Alexander <laninsky@gmail.com>
2016-05-15 11:25:08 -0500

    Update hybrid_relationships.R

--- post-processing/hybrid_relationships.R
+++ post-processing/hybrid_relationships.R
@@ -6,9 +6,10 @@ suffixes <- as.numeric(as.matrix(read.table("suffix.txt")))
 
 lenintable <- dim(intable)[1]
 temptable <- NULL
-temptemptable <- NULL
+temptablebysample <- NULL
 
 for (j in 1:lenintable) {
+temptemptable <- NULL
 temp <- unlist(strsplit(intable[j,2],"\\("))
 for (k in 1:(length(temp))) {
 temp1 <-  unlist(strsplit(temp[k],"\\)"))[1]
@@ -18,6 +19,7 @@ temp2 <- unlist(strsplit(temp1,":"))
 temp3 <- unlist(strsplit(temp2,","))
 temp4 <- c(intable[j,1],temp3[1],temp3[3])
 temptemptable <- rbind(temptemptable,temp4)
+temptable <- rbind(temptable,temp4)
 }
 }
 colnames(temptemptable) <- NULL
@@ -34,27 +36,37 @@ temp1table <- matrix(NA,ncol=4,nrow=(dim(samplenames)[1]))
 temp1table[,1] <- temptemptable[1,1]
 temp1table[,2] <- samplenames
 
-
-
+for (l in 1:(dim(temp1table)[1])) {
+for (m in 1:(dim(temptemptable)[1])) {
+if(grepl(temp1table[l,2],temptemptable[m,2],fixed=TRUE)) {
+temp1table[l,3] <- temptemptable[m,3]
+}
+if(grepl(temp1table[l,2],temptemptable[m,3],fixed=TRUE)) {
+temp1table[l,4] <- temptemptable[m,2]
+}
+}
+}
+temptablebysample <- rbind(temptablebysample,temp1table)
+}
 
 
 ######## UP TO HERE RECODING ###########
-
-
-rbind(,sumtemptable$V3)
-
-
-}
-
 colnames(temptable) <- NULL
 rownames(temptable) <- NULL
+colnames(temptablebysample) <- NULL
+rownames(temptablebysample) <- NULL
 
 sumtable <- as.data.table(temptable)[,c(2,3) := list(pmin(V2, V3), pmax(V2, V3))]
-
+sumtable$V2 <- substr(sumtable$V2,1, nchar(sumtable$V2)-suffixes)
+sumtable$V3 <- substr(sumtable$V3,1, nchar(sumtable$V3)-suffixes)
 out <- ddply(sumtable,.(V2,V3),nrow)
-
-write.table(sumtable, "allele_combinations_by_locus.txt",quote=FALSE, col.names=FALSE,row.names=FALSE)
 write.table(out, "allele_combinations.txt",quote=FALSE, col.names=FALSE,row.names=FALSE)
 
-q()
+sumtable <- as.data.table(temptablebysample)[,c(3,4) := list(pmin(V3, V4), pmax(V3, V4))]
+sumtable$V2 <- substr(sumtable$V3,1, nchar(sumtable$V3)-suffixes)
+sumtable$V3 <- substr(sumtable$V4,1, nchar(sumtable$V4)-suffixes)
+out <- ddply(sumtable,.(V3,V4),nrow)
+write.table(sumtable, "allele_combinations_per_locus.txt",quote=FALSE, col.names=FALSE,row.names=FALSE)
+write.table(out, "allele_combinations_per_locus_summary.txt",quote=FALSE, col.names=FALSE,row.names=FALSE)
 
+q()
